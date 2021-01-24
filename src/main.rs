822b40007ecf8bc76e4c98d1e708eed5eaffa207
psyashes <43512814+psyashes@users.noreply.github.com>
2021-01-25 00:36:44 +0900

    Add udp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+use std::thread;
+use std::str;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    let mut buf = [0; 100];
+
+    loop {
+      match socket.recv_from(&mut buf) {
+        Ok((buf_size, src_addr)) => {
+          thread::spawn(move || {
+            let buf = &mut buf[..buf_size];
+            let req_msg = str::from_utf8(&buf).unwrap();
+            println!("{:}", "=".repeat(80));
+            println!("buffer size: {:?}", buf_size);
+            println!("src address: {:?}", src_addr);
+            println!("request message: {:?}", req_msg);
+
+            // let res_msg = b"Thanks for sending message!";
+            // socket.send_to(res_msg, &src_addr).expect("couldn't send data");
+          });
+        },
+        Err(e) => {
+          println!("couldn't recieve request: {:?}", e);
+        }
+      }
+    }
 }
+
